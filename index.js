3ed050410037edbd19d97bd7ee8fe0393b03d2e6
Jatin Singh <70967350+beatenstratblues@users.noreply.github.com>
2025-11-07 23:54:49 +0530

    API responses edited

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const { convertImageFunction } = require("./utility/imageConversionFunction");
 const { compressImageFunction } = require("./utility/imageCompressionFunction");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"],
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ status: "ok", message: "Welcome to ConvertZee API Server!" });
@@ -38,6 +40,7 @@ app.post("/api/compress/image", async (req, res) => {
   const respo = await compressImageFunction(imageUrl, options);
 
   res.json({
+    status: respo.status,
     message: respo.message,
     compressedImageUrl: respo.optimizedUrl,
     compressionRatio: respo.compressionRatio,
